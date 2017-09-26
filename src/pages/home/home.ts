6091e129db2df81da2197885d2f805f9a49216c7
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-09-26 21:00:35 +0700

    Show popular movies

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TmdbProvider } from '../../providers/tmdb/tmdb';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private movies: Array<object> = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private tmdbProvider: TmdbProvider) {
+  }
 
+  ionViewDidLoad() {
+    this.getPopularMovies();
+  }
+
+  getPopularMovies() {
+    this.tmdbProvider.getPopularMovies().subscribe(data => {
+      this.movies = data;
+    });
   }
 
 }
