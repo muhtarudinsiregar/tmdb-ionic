c3abbec7acd10b5134fc0cb550649623514a4e91
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-10-03 21:16:33 +0700

    Added skeleton similar movies

--- src/pages/home-detail/home-detail.ts
+++ src/pages/home-detail/home-detail.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController, NavParams } from "ionic-angular";
+import { TmdbProvider } from "../../providers/tmdb/tmdb";
 
 /**
  * Generated class for the HomeDetailPage page.
@@ -9,15 +10,29 @@ import { NavController, NavParams } from 'ionic-angular';
  */
 
 @Component({
-  selector: 'page-home-detail',
-  templateUrl: 'home-detail.html',
+  selector: "page-home-detail",
+  templateUrl: "home-detail.html"
 })
 export class HomeDetailPage {
-  movie: object = {};
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  movie: any = {};
+  similarMovies: Array<any> = [];
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public tmdbApi: TmdbProvider
+  ) {
     this.movie = navParams.get("movie");
   }
 
-  ionViewDidLoad() { }
+  ionViewDidLoad() {
+    const movieId = this.movie.id;
+    this.tmdbApi.getSimilarMovies(movieId).subscribe(res => {
+      this.similarMovies = res;
+    });
+  }
 
+  posterUrl(url: string) {
+    return `https://image.tmdb.org/t/p/w780/${url}`;
+  }
 }
--- src/providers/tmdb/tmdb.ts
+++ src/providers/tmdb/tmdb.ts
@@ -1,21 +1,23 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
-import { ApiProvider } from '../api/api';
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import "rxjs/add/operator/map";
+import { ApiProvider } from "../api/api";
 
 @Injectable()
 export class TmdbProvider {
-
   constructor(public http: Http, public api: ApiProvider) {
-    console.log('Hello TmdbProvider Provider');
+    console.log("Hello TmdbProvider Provider");
   }
 
   getPopularMovies() {
-    return this.api.get('/movie/popular')
-      .map(data => data.results);
+    return this.api.get("/movie/popular").map(data => data.results);
   }
 
   getUpcomingMovies() {
-    return this.api.get('/movie/upcoming').map(data => data.results);
+    return this.api.get("/movie/upcoming").map(data => data.results);
+  }
+
+  getSimilarMovies(movieId: number) {
+    return this.api.get(`/movie/${movieId}/similar`).map(data => data.results);
   }
 }
